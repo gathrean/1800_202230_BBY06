6159dda577033ae5ee6518000f766435e3eb1ef8
Gathrean Dela Cruz <gathrean@icloud.com>
2022-11-16 17:04:52 -0800

    a lot more housekeeping and changing the looks of ScheDue

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,254 +1,112 @@
-var calendar = document.getElementById("calendar-table");
-var gridTable = document.getElementById("table-body");
-var currentDate = new Date();
-var selectedDate = currentDate;
-var selectedDayBlock = null;
-var globalEventObj = {};
+document.addEventListener('DOMContentLoaded', function() {
+var calendarEl = document.getElementById('calendar');
 
-var sidebar = document.getElementById("sidebar");
+var calendar = new FullCalendar.Calendar(calendarEl, {
+plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list', 'bootstrap',],
+weekNumbers: true,
+timeZone: 'PST',
+editable: true,
+eventLimit: true,
+selectable: true,
+eventClick: function(info) {
+  info.jsEvent.preventDefault();
 
-function createCalendar(date, side) {
-   var currentDate = date;
-   var startDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
-
-   var monthTitle = document.getElementById("month-name");
-   var monthName = currentDate.toLocaleString("en-US", {
-      month: "long"
-   });
-   var yearNum = currentDate.toLocaleString("en-US", {
-      year: "numeric"
-   });
-   monthTitle.innerHTML = `${monthName} ${yearNum}`;
-
-   if (side == "left") {
-      gridTable.className = "animated fadeOutRight";
-   } else {
-      gridTable.className = "animated fadeOutLeft";
-   }
-
-   setTimeout(() => {
-      gridTable.innerHTML = "";
-
-      var newTr = document.createElement("div");
-      newTr.className = "row";
-      var currentTr = gridTable.appendChild(newTr);
-
-      for (let i = 1; i < (startDate.getDay() || 7); i++) {
-         let emptyDivCol = document.createElement("div");
-         emptyDivCol.className = "col empty-day";
-         currentTr.appendChild(emptyDivCol);
-      }
-
-      var lastDay = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
-      lastDay = lastDay.getDate();
-
-      for (let i = 1; i <= lastDay; i++) {
-         if (currentTr.children.length >= 7) {
-            currentTr = gridTable.appendChild(addNewRow());
-         }
-         let currentDay = document.createElement("div");
-         currentDay.className = "col";
-         if (selectedDayBlock == null && i == currentDate.getDate() || selectedDate.toDateString() == new Date(currentDate.getFullYear(), currentDate.getMonth(), i).toDateString()) {
-            selectedDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), i);
-
-            document.getElementById("eventDayName").innerHTML = selectedDate.toLocaleString("en-US", {
-               month: "long",
-               day: "numeric",
-               year: "numeric"
-            });
-
-            selectedDayBlock = currentDay;
-            setTimeout(() => {
-               currentDay.classList.add("blue");
-               currentDay.classList.add("lighten-3");
-            }, 900);
-         }
-         currentDay.innerHTML = i;
-
-         //show marks
-         if (globalEventObj[new Date(currentDate.getFullYear(), currentDate.getMonth(), i).toDateString()]) {
-            let eventMark = document.createElement("div");
-            eventMark.className = "day-mark";
-            currentDay.appendChild(eventMark);
-         }
-
-         currentTr.appendChild(currentDay);
-      }
-
-      for (let i = currentTr.getElementsByTagName("div").length; i < 7; i++) {
-         let emptyDivCol = document.createElement("div");
-         emptyDivCol.className = "col empty-day";
-         currentTr.appendChild(emptyDivCol);
-      }
-
-      if (side == "left") {
-         gridTable.className = "animated fadeInLeft";
-      } else {
-         gridTable.className = "animated fadeInRight";
-      }
-
-      function addNewRow() {
-         let node = document.createElement("div");
-         node.className = "row";
-         return node;
-      }
-
-   }, !side ? 0 : 270);
-}
-
-createCalendar(currentDate);
-
-var todayDayName = document.getElementById("todayDayName");
-todayDayName.innerHTML = "Today is " + currentDate.toLocaleString("en-US", {
-   weekday: "long",
-   day: "numeric",
-   month: "short"
+  if(info.event.url) {
+    window.open(info.event.url);
+  } else {
+    SVGFEDropShadowElement.fire(info.event.title, 'Start: '+info.event.start, 'question');
+  }
+},
+droppable: true,
+drop: function(info) {
+  if(checkbox.checked) {
+    info.draggedEl.parentNode.removeChild(info.draggedEl);
+  }
+},
+header: {
+  left: 'prev,next today',
+  center: 'title',
+  right: 'dayGridMonth, timeGridWeek, timeGridDay',
+},
+events: [
+  {
+    title: 'Dinner with Jay Zed',
+    start: '2022-11-16'
+  },
+  {
+    title: 'Long Event',
+    start: '2020-02-07',
+    end: '2020-02-10',
+  },
+  {
+    groupId: 999,
+    title: 'Repeating Event',
+    start: '2022-02-09T16:00:00'
+  },
+  {
+    groupId: 999,
+    title: 'Repeating Event',
+    start: '2020-02-16T16:00:00'
+  },
+  {
+    title: 'Conference',
+    start: '2020-02-11',
+    end: '2020-02-13'
+  },
+  {
+    title: 'Meeting',
+    start: '2020-02-12T10:30:00',
+    end: '2020-02-12T12:30:00'
+  },
+  {
+    title: 'Lunch',
+    start: '2020-02-12T12:00:00'
+  },
+  {
+    title: 'Meeting',
+    start: '2020-02-12T14:30:00'
+  },
+  {
+    title: 'Happy Hour',
+    start: '2020-02-12T17:30:00'
+  },
+  {
+    title: 'Dinner',
+    start: '2020-02-12T20:00:00'
+  },
+  {
+    title: 'Birthday Party',
+    start: '2020-02-13T07:00:00'
+  },
+  {
+    title: 'Click for Google',
+    url: 'http://google.com/',
+    start: '2020-02-28'
+  }
+]
 });
 
-var prevButton = document.getElementById("prev");
-var nextButton = document.getElementById("next");
+calendar.render();
+});
 
-prevButton.onclick = function changeMonthPrev() {
-   currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1);
-   createCalendar(currentDate, "left");
-}
-nextButton.onclick = function changeMonthNext() {
-   currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1);
-   createCalendar(currentDate, "right");
-}
+var events = document.getElementById('events');
+var checkbox = document.getElementById('move-event');
+var input = document.getElementById('input-event');
+var button = document.getElementById('add-event');
 
-function addEvent(title, desc) {
-   if (!globalEventObj[selectedDate.toDateString()]) {
-      globalEventObj[selectedDate.toDateString()] = {};
-   }
-   globalEventObj[selectedDate.toDateString()][title] = desc;
-}
+new FullCalendarInteraction.Draggable(events, {
+  itemSelector: 'fc-event',
+  eventData: function(e) {
+    return {
+      title: e.innerText
+    };
+  }
+})
 
-function showEvents() {
-   let sidebarEvents = document.getElementById("sidebarEvents");
-   let objWithDate = globalEventObj[selectedDate.toDateString()];
-
-   sidebarEvents.innerHTML = "";
-
-   if (objWithDate) {
-      let eventsCount = 0;
-      for (key in globalEventObj[selectedDate.toDateString()]) {
-         let eventContainer = document.createElement("div");
-         eventContainer.className = "eventCard";
-
-         let eventHeader = document.createElement("div");
-         eventHeader.className = "eventCard-header";
-
-         let eventDescription = document.createElement("div");
-         eventDescription.className = "eventCard-description";
-
-         eventHeader.appendChild(document.createTextNode(key));
-         eventContainer.appendChild(eventHeader);
-
-         eventDescription.appendChild(document.createTextNode(objWithDate[key]));
-         eventContainer.appendChild(eventDescription);
-
-         let markWrapper = document.createElement("div");
-         markWrapper.className = "eventCard-mark-wrapper";
-         let mark = document.createElement("div");
-         mark.classList = "eventCard-mark";
-         markWrapper.appendChild(mark);
-         eventContainer.appendChild(markWrapper);
-
-         sidebarEvents.appendChild(eventContainer);
-
-         eventsCount++;
-      }
-      let emptyFormMessage = document.getElementById("emptyFormTitle");
-      emptyFormMessage.innerHTML = `${eventsCount} events now`;
-   } else {
-      let emptyMessage = document.createElement("div");
-      emptyMessage.className = "empty-message";
-      emptyMessage.innerHTML = "Sorry, no events to selected date";
-      sidebarEvents.appendChild(emptyMessage);
-      let emptyFormMessage = document.getElementById("emptyFormTitle");
-      emptyFormMessage.innerHTML = "No events now";
-   }
-}
-
-gridTable.onclick = function (e) {
-
-   if (!e.target.classList.contains("col") || e.target.classList.contains("empty-day")) {
-      return;
-   }
-
-   if (selectedDayBlock) {
-      if (selectedDayBlock.classList.contains("blue") && selectedDayBlock.classList.contains("lighten-3")) {
-         selectedDayBlock.classList.remove("blue");
-         selectedDayBlock.classList.remove("lighten-3");
-      }
-   }
-   selectedDayBlock = e.target;
-   selectedDayBlock.classList.add("blue");
-   selectedDayBlock.classList.add("lighten-3");
-
-   selectedDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), parseInt(e.target.innerHTML));
-
-   showEvents();
-
-   document.getElementById("eventDayName").innerHTML = selectedDate.toLocaleString("en-US", {
-      month: "long",
-      day: "numeric",
-      year: "numeric"
-   });
-
-}
-
-var changeFormButton = document.getElementById("changeFormButton");
-var addForm = document.getElementById("addForm");
-changeFormButton.onclick = function (e) {
-   addForm.style.top = 0;
-}
-
-var cancelAdd = document.getElementById("cancelAdd");
-cancelAdd.onclick = function (e) {
-   addForm.style.top = "100%";
-   let inputs = addForm.getElementsByTagName("input");
-   for (let i = 0; i < inputs.length; i++) {
-      inputs[i].value = "";
-   }
-   let labels = addForm.getElementsByTagName("label");
-   for (let i = 0; i < labels.length; i++) {
-      labels[i].className = "";
-   }
-}
-
-var addEventButton = document.getElementById("addEventButton");
-addEventButton.onclick = function (e) {
-   let title = document.getElementById("eventTitleInput").value.trim();
-   let desc = document.getElementById("eventDescInput").value.trim();
-
-   if (!title || !desc) {
-      document.getElementById("eventTitleInput").value = "";
-      document.getElementById("eventDescInput").value = "";
-      let labels = addForm.getElementsByTagName("label");
-      for (let i = 0; i < labels.length; i++) {
-         labels[i].className = "";
-      }
-      return;
-   }
-
-   addEvent(title, desc);
-   showEvents();
-
-   if (!selectedDayBlock.querySelector(".day-mark")) {
-      selectedDayBlock.appendChild(document.createElement("div")).className = "day-mark";
-   }
-
-   let inputs = addForm.getElementsByTagName("input");
-   for (let i = 0; i < inputs.length; i++) {
-      inputs[i].value = "";
-   }
-   let labels = addForm.getElementsByTagName("label");
-   for (let i = 0; i < labels.length; i++) {
-      labels[i].className = "";
-   }
-
-}
-
-$('.collapse').collapse()
+button.addEventListener('click', function(){
+  var div = document.createElement('div');
+  var inval = document.createTextNode(input.value);
+  div.className = 'fc-event';
+  div.appendChild(inval);
+  events.appendChild(div);
+});
