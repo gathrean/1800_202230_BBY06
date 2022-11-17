7446b8a1cee24dbb55a4f500bc34d6ef8d611aad
Gathrean Dela Cruz <gathrean@icloud.com>
2022-11-16 19:19:20 -0800

    calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,92 +1,92 @@
-document.addEventListener('DOMContentLoaded', function() {
-var calendarEl = document.getElementById('calendar');
+document.addEventListener('DOMContentLoaded', function () {
+  var calendarEl = document.getElementById('calendar');
 
-var calendar = new FullCalendar.Calendar(calendarEl, {
-plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list', 'bootstrap',],
-weekNumbers: true,
-timeZone: 'PST',
-editable: true,
-eventLimit: true,
-selectable: true,
-eventClick: function(info) {
-  info.jsEvent.preventDefault();
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    plugins: ['interaction', 'dayGrid', 'timeGrid', 'list', 'bootstrap',],
+    weekNumbers: true,
+    timeZone: 'PST',
+    editable: true,
+    eventLimit: true,
+    selectable: true,
+    eventClick: function (info) {
+      info.jsEvent.preventDefault();
 
-  if(info.event.url) {
-    window.open(info.event.url);
-  } else {
-    SVGFEDropShadowElement.fire(info.event.title, 'Start: '+info.event.start, 'question');
-  }
-},
-droppable: true,
-drop: function(info) {
-  if(checkbox.checked) {
-    info.draggedEl.parentNode.removeChild(info.draggedEl);
-  }
-},
-header: {
-  left: 'prev,next today',
-  center: 'title',
-  right: 'dayGridMonth, timeGridWeek, timeGridDay',
-},
-events: [
-  {
-    title: 'Dinner with Jay Zed',
-    start: '2022-11-16'
-  },
-  {
-    title: 'Long Event',
-    start: '2020-02-07',
-    end: '2020-02-10',
-  },
-  {
-    groupId: 999,
-    title: 'Repeating Event',
-    start: '2022-02-09T16:00:00'
-  },
-  {
-    groupId: 999,
-    title: 'Repeating Event',
-    start: '2020-02-16T16:00:00'
-  },
-  {
-    title: 'Conference',
-    start: '2020-02-11',
-    end: '2020-02-13'
-  },
-  {
-    title: 'Meeting',
-    start: '2020-02-12T10:30:00',
-    end: '2020-02-12T12:30:00'
-  },
-  {
-    title: 'Lunch',
-    start: '2020-02-12T12:00:00'
-  },
-  {
-    title: 'Meeting',
-    start: '2020-02-12T14:30:00'
-  },
-  {
-    title: 'Happy Hour',
-    start: '2020-02-12T17:30:00'
-  },
-  {
-    title: 'Dinner',
-    start: '2020-02-12T20:00:00'
-  },
-  {
-    title: 'Birthday Party',
-    start: '2020-02-13T07:00:00'
-  },
-  {
-    title: 'Click for Google',
-    url: 'http://google.com/',
-    start: '2020-02-28'
-  }
-]
-});
+      if (info.event.url) {
+        window.open(info.event.url);
+      } else {
+        SVGFEDropShadowElement.fire(info.event.title, 'Start: ' + info.event.start, 'question');
+      }
+    },
+    droppable: true,
+    drop: function (info) {
+      if (checkbox.checked) {
+        info.draggedEl.parentNode.removeChild(info.draggedEl);
+      }
+    },
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth, timeGridWeek, timeGridDay',
+    },
+    events: [
+      {
+        title: 'Dinner with JAY-Z',
+        start: '2022-11-16'
+      },
+      {
+        title: 'Long Event',
+        start: '2020-02-07',
+        end: '2020-02-10',
+      },
+      {
+        groupId: 999,
+        title: 'Repeating Event',
+        start: '2022-02-09T16:00:00'
+      },
+      {
+        groupId: 999,
+        title: 'Repeating Event',
+        start: '2020-02-16T16:00:00'
+      },
+      {
+        title: 'Conference',
+        start: '2020-02-11',
+        end: '2020-02-13'
+      },
+      {
+        title: 'Meeting',
+        start: '2020-02-12T10:30:00',
+        end: '2020-02-12T12:30:00'
+      },
+      {
+        title: 'Lunch',
+        start: '2020-02-12T12:00:00'
+      },
+      {
+        title: 'Meeting',
+        start: '2020-02-12T14:30:00'
+      },
+      {
+        title: 'Happy Hour',
+        start: '2020-02-12T17:30:00'
+      },
+      {
+        title: 'Dinner',
+        start: '2020-02-12T20:00:00'
+      },
+      {
+        title: 'Birthday Party',
+        start: '2020-02-13T07:00:00'
+      },
+      {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2022-11-29'
+      }
+    ]
+  });
 
-calendar.render();
+  calendar.render();
 });
 
 var events = document.getElementById('events');
@@ -96,14 +96,14 @@ var button = document.getElementById('add-event');
 
 new FullCalendarInteraction.Draggable(events, {
   itemSelector: 'fc-event',
-  eventData: function(e) {
+  eventData: function (e) {
     return {
       title: e.innerText
     };
   }
 })
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
   var div = document.createElement('div');
   var inval = document.createTextNode(input.value);
   div.className = 'fc-event';
