8715a49764237a536aac5861e1c1a897d2ac23da
jamesnll <langillejames11@gmail.com>
2022-11-17 21:37:54 -0800

    Added a button to create an event, submit currently doesn't work to add to the calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var calendarEl = document.getElementById('calendar');
+  var calendarObj = document.getElementById('calendar');
 
-  var calendar = new FullCalendar.Calendar(calendarEl, {
+  var calendar = new FullCalendar.Calendar(calendarObj, {
+
     plugins: ['interaction', 'dayGrid', 'timeGrid', 'list', 'bootstrap',],
     weekNumbers: true,
     timeZone: 'PST',
@@ -23,11 +24,17 @@ document.addEventListener('DOMContentLoaded', function () {
         info.draggedEl.parentNode.removeChild(info.draggedEl);
       }
     },
+
     header: {
       left: 'prev,next today',
       center: 'title',
       right: 'dayGridMonth, timeGridWeek, timeGridDay',
     },
+
+    footer: {
+
+    },
+
     events: [
       {
         title: 'Dinner with JAY-Z',
@@ -47,36 +54,6 @@ document.addEventListener('DOMContentLoaded', function () {
         groupId: 999,
         title: 'Repeating Event',
         start: '2020-02-16T16:00:00'
-      },
-      {
-        title: 'Conference',
-        start: '2020-02-11',
-        end: '2020-02-13'
-      },
-      {
-        title: 'Meeting',
-        start: '2020-02-12T10:30:00',
-        end: '2020-02-12T12:30:00'
-      },
-      {
-        title: 'Lunch',
-        start: '2020-02-12T12:00:00'
-      },
-      {
-        title: 'Meeting',
-        start: '2020-02-12T14:30:00'
-      },
-      {
-        title: 'Happy Hour',
-        start: '2020-02-12T17:30:00'
-      },
-      {
-        title: 'Dinner',
-        start: '2020-02-12T20:00:00'
-      },
-      {
-        title: 'Birthday Party',
-        start: '2020-02-13T07:00:00'
       },
       {
         title: 'Click for Google',
