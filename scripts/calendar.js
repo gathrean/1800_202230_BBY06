92960b2ae202ec1b1d984d469ac8d551a26518fd
jamesnll <langillejames11@gmail.com>
2022-11-26 11:44:47 -0800

    Changed JS code for adding a calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -41,236 +41,394 @@ function getEventInfo() {
           title: nameEvent,
           start: startEvent,
           end: endEvent
-        })
+        });
+        console.log(nameEvent, startEvent, endEvent);
       })
     })
 }
-
-getEventInfo()
-
-// Second calendar for when the carousel works properly
+// Creates the school calendar
 document.addEventListener('DOMContentLoaded', function () {
-  var calendarObj2 = document.getElementById('calendar2');
-
-  var schoolCalendar = new FullCalendar.Calendar(calendarObj2, {
-
-    plugins: ['interaction', 'dayGrid', 'timeGrid', 'list', 'bootstrap',],
-    weekNumbers: true,
-    timeZone: 'PST',
-    editable: true,
-    eventLimit: true,
-    selectable: true,
-    eventClick: function (info) {
-      info.jsEvent.preventDefault();
-
-      if (info.event.url) {
-        window.open(info.event.url);
-      } else {
-        Swal.fire(info.event.title, 'Start: ' + info.event.start, 'question');
-      }
-    },
-    droppable: true,
-    drop: function (info) {
-      if (checkbox.checked) {
-        info.draggedEl.parentNode.removeChild(info.draggedEl);
-      }
-    },
-
-    customButtons: {
-      addEventButton: {
-        text: 'Add Event',
-        click: function() {
-          var dateStr = prompt('Enter a date in YYYY-MM-DD format');
-          var date = new Date(dateStr + 'T00:00:00'); // will be in local time
-
-          if (!isNaN(date.valueOf())) { // valid?
-            calendar.addEvent({
-              title: 'dynamic event',
-              start: date,
-              allDay: true
-            });
-      }
-    }
-    }
-  },
-
-    header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth, timeGridWeek, timeGridDay',
-    },
-
-    footer: {
-      center: 'addEventButton'
-    },
-
-    events: [
-      {
-        title: 'TEST',
-        start: '2022-11-11',
-        end: '2022-11-12'
+  // Gets id='schoolCalendar'
+  var calendarEl = document.getElementById('schoolCalendar');
+  // Calendar generation
+  var schoolCalendar = new FullCalendar.Calendar(calendarEl, {
+      // Adds buttons to the calendar header
+      headerToolbar: {
+          left: 'prevYear,prev,next,nextYear',
+          center: 'title',
+          right: 'today,dayGridMonth,dayGridWeek,dayGridDay'
       },
-      {
-        title: 'Long Event',
-        start: '2020-02-07',
-        end: '2020-02-10',
+      // Adds buttons to the footer
+      footerToolbar: {
+        left: 'addEventButton' 
       },
-      {
-        groupId: 999,
-        title: 'Repeating Event',
-        start: '2022-02-09T16:00:00'
+      // Code needed to create the add event button
+      customButtons: {
+          addEventButton: {
+              // Title of button
+              text: 'Add Event',
+              click: function () {
+                  var dateStr = prompt('Enter the start date in YYYY-MM-DD format');
+                  var date = new Date(dateStr + 'T00:00:00'); // will be in local time
+                  var dateEnd = prompt('Enter the end date in YYYY-MM-DD format');
+                  var dateE = new Date(dateEnd + 'T00:00:00'); // will be in local time
+
+                  if (!isNaN(date.valueOf())) { // valid?
+                    // Adds the event to the calendar
+                      schoolCalendar.addEvent({
+                          title: 'dynamic event',
+                          start: date,
+                          end: dateE,
+                          allDay: true
+                      });
+                  }
+              }
+          }
       },
-      {
-        groupId: 999,
-        title: 'Repeating Event',
-        start: '2020-02-16T16:00:00'
-      },
-      {
-        title: 'Click for Google',
-        url: 'http://google.com/',
-        start: '2022-11-29'
-      }
-    ]
+      // Sets the timezone to pst
+      timeZone: 'PST',
+      navLinks: true, // can click day/week names to navigate views
+      editable: true,
+      dayMaxEvents: true, // allow "more" link when too many events
+      selectable: true, // select calendar cells
+
+      // preset calendar events
+      events: [
+          {
+              title: 'All Day Event',
+              start: '2022-1-01'
+          },
+          {
+              title: 'Long Event',
+              start: '2022-11-07',
+              end: '2020-09-10'
+          },
+          {
+              groupId: 999,
+              title: 'Repeating Event',
+              start: '2022-11-09T16:00:00'
+          },
+          {
+              groupId: 999,
+              title: 'Repeating Event',
+              start: '2022-11-16T16:00:00'
+          },
+          {
+              title: 'Conference',
+              start: '2020-09-11',
+              end: '2020-09-13'
+          },
+          {
+              title: 'Meeting',
+              start: '2020-09-12T10:30:00',
+              end: '2020-09-12T12:30:00'
+          },
+          {
+              title: 'Lunch',
+              start: '2020-09-12T12:00:00'
+          },
+          {
+              title: 'Meeting',
+              start: '2020-09-12T14:30:00'
+          },
+          {
+              title: 'Happy Hour',
+              start: '2020-09-12T17:30:00'
+          },
+          {
+              title: 'Dinner',
+              start: '2020-09-12T20:00:00'
+          },
+          {
+              title: 'Birthday Party',
+              start: '2020-09-13T07:00:00'
+          },
+          {
+              title: 'Click for Google',
+              url: 'http://google.com/',
+              start: '2020-09-28'
+          }
+      ]
   });
-  
   schoolCalendar.render();
 });
-// Calendar for when carousel works correctly
+
+// Second calendar for when carousel works
 document.addEventListener('DOMContentLoaded', function () {
-  var calendarObj3 = document.getElementById('calendar3');
-
-  var workCalendar = new FullCalendar.Calendar(calendarObj3, {
-
-    plugins: ['interaction', 'dayGrid', 'timeGrid', 'list', 'bootstrap',],
-    weekNumbers: true,
-    timeZone: 'PST',
-    editable: true,
-    eventLimit: true,
-    selectable: true,
-    eventClick: function (info) {
-      info.jsEvent.preventDefault();
-
-      if (info.event.url) {
-        window.open(info.event.url);
-      } else {
-        Swal.fire(info.event.title, 'Start: ' + info.event.start, 'question');
-      }
-    },
-    droppable: true,
-    drop: function (info) {
-      if (checkbox.checked) {
-        info.draggedEl.parentNode.removeChild(info.draggedEl);
-      }
-    },
-
-    header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth, timeGridWeek, timeGridDay',
-    },
-
-    footer: {
-
-    },
-
-    events: [
-      {
-        title: eventName,
-        start: eventStart,
-        end: eventEnd
-      },
-      {
-        title: 'Long Event',
-        start: '2020-02-07',
-        end: '2020-02-10',
-      },
-      {
-        groupId: 999,
-        title: 'Repeating Event',
-        start: '2022-02-09T16:00:00'
-      },
-      {
-        groupId: 999,
-        title: 'Repeating Event',
-        start: '2020-02-16T16:00:00'
-      },
-      {
-        title: 'Click for Google',
-        url: 'http://google.com/',
-        start: '2022-11-29'
-      }
-    ]
-  });
+    var calendarEl2 = document.getElementById('calendar3');
   
-  workCalendar.render();
-});
-
-// First calendar made
-document.addEventListener('DOMContentLoaded', function () {
-  var calendarObj = document.getElementById('calendar');
-
-  var calendar = new FullCalendar.Calendar(calendarObj, {
-
-    plugins: ['interaction', 'dayGrid', 'timeGrid', 'list', 'bootstrap',],
-    weekNumbers: true,
-    timeZone: 'PST',
-    editable: true,
-    eventLimit: true,
-    selectable: true,
-    eventClick: function (info) {
-      info.jsEvent.preventDefault();
-
-      if (info.event.url) {
-        window.open(info.event.url);
-      } else {
-        Swal.fire(info.event.title, 'Start: ' + info.event.start, 'question');
-      }
-    },
-    droppable: true,
-    drop: function (info) {
-      if (checkbox.checked) {
-        info.draggedEl.parentNode.removeChild(info.draggedEl);
-      }
-    },
-
-    header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth, timeGridWeek, timeGridDay',
-    },
-
-    footer: {
-
-    },
-
-    events: [
-      {
-        title: 'TEST',
-        start: '2022-11-28',
-        end: '2022-11-30'
-      },
-      {
-        title: 'Long Event',
-        start: '2020-02-07',
-        end: '2020-02-10',
-      },
-      {
-        groupId: 999,
-        title: 'Repeating Event',
-        start: '2022-02-09T16:00:00'
-      },
-      {
-        groupId: 999,
-        title: 'Repeating Event',
-        start: '2020-02-16T16:00:00'
-      },
-      {
-        title: 'Click for Google',
-        url: 'http://google.com/',
-        start: '2022-11-29'
-      }
-    ]
+    var calendar3 = new FullCalendar.Calendar(calendarEl2, {
+        headerToolbar: {
+            left: 'prevYear,prev,next,nextYear today addEventButton',
+            center: 'title',
+            right: 'dayGridMonth,dayGridWeek,dayGridDay'
+        },
+        customButtons: {
+            addEventButton: {
+                text: 'Add Event',
+                click: function () {
+                    var dateStr = prompt('Enter a date in YYYY-MM-DD format');
+                    var date = new Date(dateStr + 'T00:00:00'); // will be in local time
+  
+                    if (!isNaN(date.valueOf())) { // valid?
+                        calendar.addEvent({
+                            title: 'dynamic event',
+                            start: date,
+                            allDay: true
+                        });
+                    }
+                }
+            }
+        },
+        timeZone: 'PST',
+        navLinks: true, // can click day/week names to navigate views
+        editable: true,
+        dayMaxEvents: true, // allow "more" link when too many events
+  
+        events: [
+            {
+                title: 'All Day Event',
+                start: '2022-1-01'
+            },
+            {
+                title: 'Long Event',
+                start: '2022-11-07',
+                end: '2020-09-10'
+            },
+            {
+                groupId: 999,
+                title: 'Repeating Event',
+                start: '2022-11-09T16:00:00'
+            },
+            {
+                groupId: 999,
+                title: 'Repeating Event',
+                start: '2022-11-16T16:00:00'
+            },
+            {
+                title: 'Conference',
+                start: '2020-09-11',
+                end: '2020-09-13'
+            },
+            {
+                title: 'Meeting',
+                start: '2020-09-12T10:30:00',
+                end: '2020-09-12T12:30:00'
+            },
+            {
+                title: 'Lunch',
+                start: '2020-09-12T12:00:00'
+            },
+            {
+                title: 'Meeting',
+                start: '2020-09-12T14:30:00'
+            },
+            {
+                title: 'Happy Hour',
+                start: '2020-09-12T17:30:00'
+            },
+            {
+                title: 'Dinner',
+                start: '2020-09-12T20:00:00'
+            },
+            {
+                title: 'Birthday Party',
+                start: '2020-09-13T07:00:00'
+            },
+            {
+                title: 'Click for Google',
+                url: 'http://google.com/',
+                start: '2020-09-28'
+            }
+        ]
+    });
+  
+    calendar3.render();
   });
 
-  calendar.render();
-});
+// Second calendar for when the carousel works properly
+// document.addEventListener('DOMContentLoaded', function () {
+//   var calendarObj2 = document.getElementById('calendar2');
+
+//   var schoolCalendar = new FullCalendar.Calendar(calendarObj2, {
+
+//     plugins: ['interaction', 'dayGrid', 'timeGrid', 'list', 'bootstrap',],
+//     weekNumbers: true,
+//     timeZone: 'PST',
+//     editable: true,
+//     eventLimit: true,
+//     selectable: true,
+//     eventClick: function (info) {
+//       info.jsEvent.preventDefault();
+
+//       if (info.event.url) {
+//         window.open(info.event.url);
+//       } else {
+//         Swal.fire(info.event.title, 'Start: ' + info.event.start, 'question');
+//       }
+//     },
+//     droppable: true,
+//     drop: function (info) {
+//       if (checkbox.checked) {
+//         info.draggedEl.parentNode.removeChild(info.draggedEl);
+//       }
+//     },
+
+//     header: {
+//       left: 'prev,next today',
+//       center: 'title',
+//       right: 'dayGridMonth, timeGridWeek, timeGridDay',
+//     },
+
+//     footer: {
+//       center: 'addEventButton'
+//     },
+
+//     events: getEventInfo({
+//       title: nameEvent,
+//       start: startEvent,
+//       end: endEvent
+//     })
+//   });
+  
+//   schoolCalendar.render();
+// });
+// Calendar for when carousel works correctly
+// document.addEventListener('DOMContentLoaded', function () {
+//   var calendarObj3 = document.getElementById('calendar3');
+
+//   var workCalendar = new FullCalendar.Calendar(calendarObj3, {
+
+//     plugins: ['interaction', 'dayGrid', 'timeGrid', 'list', 'bootstrap',],
+//     weekNumbers: true,
+//     timeZone: 'PST',
+//     editable: true,
+//     eventLimit: true,
+//     selectable: true,
+//     eventClick: function (info) {
+//       info.jsEvent.preventDefault();
+
+//       if (info.event.url) {
+//         window.open(info.event.url);
+//       } else {
+//         Swal.fire(info.event.title, 'Start: ' + info.event.start, 'question');
+//       }
+//     },
+//     droppable: true,
+//     drop: function (info) {
+//       if (checkbox.checked) {
+//         info.draggedEl.parentNode.removeChild(info.draggedEl);
+//       }
+//     },
+
+//     header: {
+//       left: 'prev,next today',
+//       center: 'title',
+//       right: 'dayGridMonth, timeGridWeek, timeGridDay',
+//     },
+
+//     footer: {
+
+//     },
+
+//     events: [
+//       {
+//         title: eventName,
+//         start: eventStart,
+//         end: eventEnd
+//       },
+//       {
+//         title: 'Long Event',
+//         start: '2020-02-07',
+//         end: '2020-02-10',
+//       },
+//       {
+//         groupId: 999,
+//         title: 'Repeating Event',
+//         start: '2022-02-09T16:00:00'
+//       },
+//       {
+//         groupId: 999,
+//         title: 'Repeating Event',
+//         start: '2020-02-16T16:00:00'
+//       },
+//       {
+//         title: 'Click for Google',
+//         url: 'http://google.com/',
+//         start: '2022-11-29'
+//       }
+//     ]
+//   });
+  
+//   workCalendar.render();
+// });
+
+// // First calendar made
+// document.addEventListener('DOMContentLoaded', function () {
+//   var calendarObj = document.getElementById('calendar');
+
+//   var calendar = new FullCalendar.Calendar(calendarObj, {
+
+//     plugins: ['interaction', 'dayGrid', 'timeGrid', 'list', 'bootstrap',],
+//     weekNumbers: true,
+//     timeZone: 'PST',
+//     editable: true,
+//     eventLimit: true,
+//     selectable: true,
+//     eventClick: function (info) {
+//       info.jsEvent.preventDefault();
+
+//       if (info.event.url) {
+//         window.open(info.event.url);
+//       } else {
+//         Swal.fire(info.event.title, 'Start: ' + info.event.start, 'question');
+//       }
+//     },
+//     droppable: true,
+//     drop: function (info) {
+//       if (checkbox.checked) {
+//         info.draggedEl.parentNode.removeChild(info.draggedEl);
+//       }
+//     },
+
+//     header: {
+//       left: 'prev,next today',
+//       center: 'title',
+//       right: 'dayGridMonth, timeGridWeek, timeGridDay',
+//     },
+
+//     footer: {
+
+//     },
+
+//     events: [
+//       {
+//         title: 'TEST',
+//         start: '2022-11-28',
+//         end: '2022-11-30'
+//       },
+//       {
+//         title: 'Long Event',
+//         start: '2020-02-07',
+//         end: '2020-02-10',
+//       },
+//       {
+//         groupId: 999,
+//         title: 'Repeating Event',
+//         start: '2022-02-09T16:00:00'
+//       },
+//       {
+//         groupId: 999,
+//         title: 'Repeating Event',
+//         start: '2020-02-16T16:00:00'
+//       },
+//       {
+//         title: 'Click for Google',
+//         url: 'http://google.com/',
+//         start: '2022-11-29'
+//       }
+//     ]
+//   });
+
+//   calendar.render();
+// });
