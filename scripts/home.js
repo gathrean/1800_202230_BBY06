09dc3e5b1da28de305740def48317cbb12e4d1b8
jamesnll <langillejames11@gmail.com>
2022-11-27 21:00:37 -0800

    JS functions to add data from firebase to summaries

---
+++ scripts/home.js
@@ -0,0 +1,71 @@
+function populateTasks() {
+    // Checks for current user
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            let taskCardTemplate = document.getElementById("taskCardTemplate");
+            let taskCardGroup = document.getElementById("taskCardGroup");
+
+            db.collection("users").doc(user.uid).collection("tasks")
+                .orderBy("time").limit(3).get().then(allTasks => {
+                    allTasks.forEach(doc => {
+                        let taskName = doc.data().task; // Gets task field
+                        let testTaskCard = taskCardTemplate.content.cloneNode(true);
+                        testTaskCard.querySelector('.task-title').innerHTML = taskName;
+                        taskCardGroup.appendChild(testTaskCard);
+                    })
+                })
+        } else {
+            console.log("No user signed in");
+            window.location.href = "start.html"
+        }
+    });
+}
+populateTasks();
+
+function populateSchoolEvents() {
+    // Checks for current user
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            let schoolEventsCardTemplate = document.getElementById("schoolEventsTemplate");
+            let schoolEventsCardGroup = document.getElementById("schoolEventsCardGroup");
+
+            db.collection("users").doc(user.uid).collection("schoolCalendar")
+                .orderBy("start").limit(3).get().then(allTasks => {
+                    allTasks.forEach(doc => {
+                        let eventName = doc.data().title; // Gets title field
+                        let schoolEventsCard = schoolEventsCardTemplate.content.cloneNode(true);
+                        schoolEventsCard.querySelector('.schoolEvent-title').innerHTML = eventName;
+                        schoolEventsCardGroup.appendChild(schoolEventsCard);
+                    })
+                })
+        } else {
+            console.log("No user signed in");
+            window.location.href = "start.html"
+        }
+    });
+}
+populateSchoolEvents();
+
+function populateWorkEvents() {
+    // Checks for current user
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            let workEventsCardTemplate = document.getElementById("workEventsTemplate");
+            let workEventsCardGroup = document.getElementById("workEventsCardGroup");
+
+            db.collection("users").doc(user.uid).collection("workCalendar")
+                .orderBy("start").limit(3).get().then(allTasks => {
+                    allTasks.forEach(doc => {
+                        let eventName = doc.data().title; // Gets title field
+                        let workEventsCard = workEventsCardTemplate.content.cloneNode(true);
+                        workEventsCard.querySelector('.workEvent-title').innerHTML = eventName;
+                        workEventsCardGroup.appendChild(workEventsCard);
+                    })
+                })
+        } else {
+            console.log("No user signed in");
+            window.location.href = "start.html"
+        }
+    });
+}
+populateWorkEvents();
