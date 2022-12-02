c41245e6f7c67e9eee7f205701178b2b9ad9e7c8
Gathrean Dela Cruz <gathrean@icloud.com>
2022-12-01 20:18:54 -0800

    commit

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,9 +3,9 @@
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
 function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('/info/nav.html'));
-    console.log($('#navbar0Placeholder').load('/info/nav0.html'));
-    console.log($('#footerPlaceholder').load('/info/footer.html'));
-    console.log($('#task-manager').load('/run/run-tasks.html'));
+    console.log($('#navbarPlaceholder').load('./info/nav.html'));
+    console.log($('#navbar0Placeholder').load('./info/nav0.html'));
+    console.log($('#footerPlaceholder').load('./info/footer.html'));
+    console.log($('#task-manager').load('./run/run-tasks.html'));
 }
 loadSkeleton();  //invoke the function
